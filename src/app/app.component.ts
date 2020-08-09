7bd173a7a241d51750bd816a72ac89ceed47aa24
Akhil shaji <akhilshaji86@gmail.com>
2020-08-09 14:05:32 +0530

    validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,14 +10,24 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'mailer-app';
   toMailString: string;
+  password: string;
   fromMailId: string;
-  subjectOfMail: any;
-  mailContent: any;
-  constructor(private http: HttpClient) {
-
+  subjectOfMail: string;
+  mailContent: string;
+  emailForm: FormGroup;
+  constructor(private http: HttpClient, formBuilder: FormBuilder) {
+    this.emailForm = formBuilder.group({
+      fromEmail: ['', Validators.compose(
+        [Validators.required, Validators.pattern('^[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}$'), Validators.minLength(1)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      recipientMailId: ['', Validators.required],
+      subject: ['', [Validators.required]],
+      content: ['', [Validators.required]],
+    });
   }
-  sendMail(form: NgForm): void {
+  onSubmit(form: NgForm): void {
     if (!form.invalid) {
+      debugger
       const mailList = this.toMailString.split(',');
       this.http.post('api/sendMail', {
         fromMail: this.fromMailId,
